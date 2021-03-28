a09684a35e7def7c6387d291a4d9917239317a0a
Siddheshwar Kumar <siddheshwar.kumar@zalando.de>
2021-03-28 21:41:25 +0200

    initial setup

---
+++ src/main/java/ai/siddha/panchang/Constants.java
@@ -0,0 +1,5 @@
+package ai.siddha.panchang;
+
+public class Constants {
+    public static final int MOON_ANGLE_DIFFERENCE_SUN = 12;
+}
---
+++ src/main/java/ai/siddha/panchang/Tithi.java
@@ -0,0 +1,7 @@
+package ai.siddha.panchang;
+
+/**
+ * This is the time during which the difference of the increment of longitude of the Sun and the Moon amounts to 12°.
+ */
+public class Tithi {
+}
