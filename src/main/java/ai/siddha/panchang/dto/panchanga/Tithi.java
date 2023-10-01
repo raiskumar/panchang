34a951d9d6b00b61cb8bcc1a22a408d21e943ae0
Siddheshwar Kumar <sikumar@zalando-72438.fritz.box>
2023-10-01 15:24:08 +0200

    documentation

--- src/main/java/ai/siddha/panchang/dto/panchanga/Tithi.java
+++ src/main/java/ai/siddha/panchang/dto/panchanga/Tithi.java
@@ -6,9 +6,16 @@ package ai.siddha.panchang.dto.panchanga;
 public class Tithi {
     private String aka = "Lunar Day";
     private String lunarMonth = "29.53 days";
+
+    //Lunar Days
+    // Calculator - https://www.birthastro.com/astrology/tithi-finder-birth-tithi-calculator
     private String[] values = {
-            "Prathma", "Dwitiya"
+            "Prathma", "Dwitiya", "Trithiya", "Chaturthi", "Panchami", "Shashti", "Saptmi", "Ashtami", "Navami", "Dakshami", "Ekadashi", "Dwadashi", "Trayodashi", "Chaturdashi", "Purnima", // Shukla Paksha
+            "Prathma", "Dwitiya", "Trithiya", "Chaturthi", "Panchami", "Shashti", "Saptmi", "Ashtami", "Navami", "Dakshami", "Ekadashi", "Dwadashi", "Trayodashi", "Chaturdashi", "Amavasya"  // Krishna Paksha
     };
     private String pakshas[] = {"Sukla Paksha", "Krishna Paksha"};
+
+    private String[]
+            vaara = {"Ravivar", "Somvar", "Mangalwar", "Budhawar", "Guruwar","Shukrawar", "Shaniwar"};
 
 }
---
+++ src/main/resources/Proposal.md
@@ -0,0 +1,12 @@
+# TithiApp 
+One app for your Sanatan way of life!  
+
+## Ideas for showing details on default screen/page
+* Show sun/moon and different aspects (waxing and waning shapes) of it. Something as shown here - https://www.bbc.com/weather/2950159
+* (Priority 2) Show temperature at the moment
+* Some way to show the gradual transition to next Tithi which also captures after how long the transition will happen.
+* Support for Native language by default. And change the language on the fly
+* Relative position of SUN, MOON and EARTH ?
+* (Priority 3) Ayurveda
+* Different festivals
+* (Priority 2) Different fasts
